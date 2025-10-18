ad573774cd97c4fec95b08287770c53a546099a7
CH-laundry <ch.laundryshop@gmail.com>
2025-10-18 14:06:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const messageHandler = require('./services/message');
 const { Client } = require('@line/bot-sdk');
 const googleAuth = require('./services/googleAuth');
 const multer = require('multer');
-const orderManager = require('./services/orderManagerDB');
-const { initDatabase } = require('./services/database');
+const orderManager = require('./services/orderManager');
 const upload = multer({ storage: multer.memoryStorage() });
 
 if (process.env.GOOGLE_PRIVATE_KEY) {
@@ -733,5 +732,6 @@ app.listen(PORT, async () => {
         }
     }, 12 * 60 * 60 * 1000);
 });
+
 
 
